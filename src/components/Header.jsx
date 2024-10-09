c0dfd07097e1d51de466a559c30adc37088b6735
Davonne <davonnejv@gmail.com>
2024-10-08 19:49:17 -0500

    update Header.jsx, add image source

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="text-6xl text-center mt-10 font-agb text-white">
+    <header className="text-6xl text-center  text-white bg-black bg-opacity-30 p-2">
       <h1>October</h1>
       <p className="text-3xl mt-4">Disney Movies Edition 2024</p>
     </header>
